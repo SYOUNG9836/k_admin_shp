052bb468cfc796367a2c0a79db5731d7727a00fc
SYOUNG9836 <youngsin0306@kookmin.ac.kr>
2024-05-16 19:12:51 +0900

    commit

--- R/data.R
+++ R/data.R
@@ -8,29 +8,29 @@ NULL
 #'
 #' @description
 #' Administrative districts of Korea shapefile, sido
-#' 
+#' @source http://www.gisdeveloper.co.kr/?p=2332
 #' @name do
 #' @export
-"do"
+NULL
 
 
 #' sgg
 #'
 #' @description
 #' Administrative districts of Korea shapefile, sigungu
-#' 
+#' @source http://www.gisdeveloper.co.kr/?p=2332
 #' @name sgg
 #' @export
-"sgg"
+NULL
 
 
 #' emd
 #'
 #' @description
 #' Administrative districts of Korea shapefile, eupmyeondong
-#' 
+#' @source http://www.gisdeveloper.co.kr/?p=2332
 #' @name emd
 #' @export
-"emd"
+NULL
 
 
